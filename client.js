6ae535513480979ec6dd187711bf5cae37c0bd9e
Ronn Ross <Ronn.ross@gmail.com>
2017-02-08 21:23:29 -0500

    updating to a choo app

--- client.js
+++ client.js
@@ -1,4 +1,7 @@
 const p = require('pico-lambda')
+var html = require('choo/html')
+var choo = require('choo')
+var app = choo()
 
 const alphabet = [
   { letter: 'k', type: "consonant", pronounced: "kill" },
@@ -117,3 +120,33 @@ const phrases = [
   { phrase: "", meaning: "" },
   { phrase: "", meaning: "" },
 ]
+
+
+
+
+app.model({
+  state: { title: 'Not quite set yet' },
+  reducers: {
+    update: function (state, data) {
+      return { title: data }
+    }
+  }
+})
+
+function mainView(state, prev, send) {
+  return html`
+    <main>
+      <h1>Title: ${state.title}</h1>
+      <input type="text" oninput=${update}>
+    </main>
+  `
+
+  function update(e) {
+    send('update', e.target.value)
+  }
+}
+
+app.router(['/', mainView])
+
+var tree = app.start()
+document.body.appendChild(tree)
